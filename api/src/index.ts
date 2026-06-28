b12bb2cf7bb135ba780021586a7f3e220de50678
utkarshj014 <utkarshj014@gmail.com>
2026-06-29 03:04:28 +0530

    Added CORS

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,34 @@
-import express from "express";
+import express, { type Application } from "express";
 import eventRoutes from "./routes/event.routes.js";
 import webhookTestRoutes from "./routes/webhook-test.routes.js";
 import deliveryRoutes from "./routes/delivery.routes.js";
 import metricRoutes from "./routes/metrics.routes.js";
+import cors, { type CorsOptions } from "cors";
 
-const app = express();
+const app: Application = express();
+
+const allowedOrigins = ["http://localhost:5173"];
+
+const corsOptions: CorsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean | undefined) => void,
+  ) => {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Hookfire_Delivery-Key"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
